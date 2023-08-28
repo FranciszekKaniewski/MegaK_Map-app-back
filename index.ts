d173c19f5d5925f9a08f41c2123330c1754b55e8
FranciszekKaniewski <fkaniewski.web.com>
2023-08-28 10:44:29 +0200

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import 'express-async-errors'
 import cors from 'cors';
+import {handleError, ValidationError} from "./utils/errors";
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.use(cors({
     origin:'http://localhost:3000',
 }));
 app.use(json());
+
+// app.get('/',async (req,res)=>{
+//     throw new ValidationError("Error!");
+// });
+
+app.use(handleError)
 
 app.listen(3001,'0.0.0.0', ()=>{
     console.log('lisening on http://localhost:3001');
